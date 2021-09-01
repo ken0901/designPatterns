29bd3a51c44a89e3b9fed999ca917ced252f156a
ken0901 <ken5260715@gmail.com>
2021-09-01 00:15:44 -0700

    commit test

--- designPatterns/src/designPatterns/adapter/Main.java
+++ designPatterns/src/designPatterns/adapter/Main.java
@@ -3,7 +3,6 @@ package designPatterns.adapter;
 public class Main {
 
 	public static void main(String[] args) {
-			
 		Adapter adapter = new AdapterImpl();
 		
 		System.out.println(adapter.twiceOf(100f));
